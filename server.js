4aae34e71479690d8564ceffa98a91c869cca100
Serena Chandler <serenachandler2019@gmail.com>
2021-04-21 20:38:19 -0700

    reorganized files to keep them in order

--- server.js
+++ server.js
@@ -11,18 +11,19 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
   });
 
 app.get("/notes", function(req, res) {
-res.sendFile(path.join(__dirname, "notes.html"));
+res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 app.get("/api/notes", function(req, res){
   return res.sendFile(path.json(__dirname, "db.json"))
 })
 
-var currentNotes = []
+
+
 
 
 
